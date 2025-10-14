15bdf60a43ec1b9153cd7ceb9ea6bd852e86f8b7
UUVA <uuva-JH@outlook.com>
2025-10-14 17:47:34 +0800

    first

---
+++ Blockchain.js
@@ -0,0 +1,143 @@
+const Block = require('./Block.js');
+const crypto = require('crypto');
+
+class Blockchain {
+
+    constructor() {
+        this.blockchain = [Block.genesis];
+        this.difficulty = 3;
+    }
+
+    get() {
+        returnthis.blockchain;
+    }
+    get latestBlock() {
+        return this.blockchain[this.blockchain.length - 1];
+    }
+
+    isValidHashDifficulty(hash) {
+        for (let i = 0; i < hash.length; i++) {
+            if (hash[i] !== '0') {
+                break;
+            }
+        }
+        return i >= this.difficulty;
+    }
+
+    calculateHashForBlock(block) {
+        const {
+            index,
+            previousHash,
+            timestamp,
+            data,
+            nonce
+        } = block;
+        return this.calculateHash(index, previousHash, timestamp, data, nonce);
+    }
+
+    calculateHash(index, previousHash, timestamp, data, nonce) {
+        return crypto.createHash('sha256').update(index + previousHash + timestamp + data + nonce).digest('hex');
+    }
+
+    mine(data) {
+        // const nextIndex = this.latestBlock.index + 1;
+        // const previousHash = this.latestBlock.hash;
+        // let timestamp = new Date().getTime();
+        // let nonce = 0;
+        // let nextHash = this.calculateHash(nextIndex, previousHash, timestamp, data, nonce);
+        const newBlock = this.generateNextBlock(data);
+        try {
+            this.addBlock(newBlock);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    generateNextBlock(data) {
+        const nextIndex = this.latestBlock.index + 1;
+        const previousHash = this.latestBlock.hash;
+        let timestamp = new Date().getTime();
+        let nonce = 0;
+        let nextHash = this.calculateHash(nextIndex, previousHash, timestamp, data, nonce);
+
+        while (!this.isValidHashDifficulty(nextHash)) {
+            nonce = nonce + 1;
+            timestamp = new Date().getTime();
+            nextHash = this.calculateHash(nextIndex, previousHash, timestamp, data, nonce);
+        }
+
+        const nextBlock = new Block(
+            nextIndex,
+            previousHash,
+            timestamp,
+            data,
+            nonce,
+            nextHash
+        );
+
+        return nextBlock;
+
+    }
+
+    // 添加区块到区块链的方法
+    addBlock(newBlock) {
+        if (this.isValidNextBlock(newBlock, this.latestBlock)) {
+            this.blockchain.push(newBlock);
+        } else {
+            throw "Error: Invalid Block";
+        }
+    }
+
+    // 验证新区快是否是
+    isValidNextBlock(nextBlock, previousBlock) {
+        const nextBlockHash = this.calculateHashForBlock(nextBlock);
+
+        if (previousBlock.index + 1 !== nextBlock.index) {
+            return false;
+        } else if (previousBlock.hash !== nextBlock.index) {
+            return false;
+        } else if (nextBlockHash !== nextBlock.hash) {
+            return false;
+        } else if (!this.isValidHashDifficulty(nextBlockHash)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // 验证整条区块链是否合法且有效
+    isValidChain(chain) {
+        if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis)) {
+            return false;
+        }
+
+        const tempChain = [chain[0]];
+
+        for (let i = 1; i < chain.length; i++) {
+            if (this.isValidNextBlock(chain[i], tempChain[i - 1])) {
+                tempChain.push(chain[i]);
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    // 检查新链是否比当前链更长
+    isChainLonger(chain) {
+        return chain.length > this.blockchain.length;
+    }
+
+    // 替换区块链的方法—，---用于处理区块链分叉，保留最长的有效链
+    replaceChain(newChain) {
+        if (this.isValidChain(newChain) && this.isChainLonger(newChain)) {
+            this.blockchain = JSON.parse(JSON.stringify(newChain));
+        } else {
+            throw "Error: Invalid Chain";
+        }
+    }
+}
+
+// 导出Blockchain类，让其他文件可以使用
+module.exports = Blockchain;
